01597b2027ab7c21d64a838a0a603d5810a3a256
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-18 15:47:25 +0000

    Welcome page and auth pages

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,11 @@
+// pages/setup.tsx
+
 import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
 const Setup = async () => {
   const profile = await initialProfile();
   const server = await db.server.findFirst({
@@ -13,6 +17,13 @@ const Setup = async () => {
       },
     },
   });
+
+  useEffect(() => {
+    const isWelcomeShown = localStorage.getItem("isWelcomeShown");
+    if (!isWelcomeShown) {
+      redirect("/welcome");
+    }
+  }, []);
 
   if (server) {
     return redirect(`/servers/${server.id}`);
@@ -26,3 +37,4 @@ const Setup = async () => {
 };
 
 export default Setup;
+
