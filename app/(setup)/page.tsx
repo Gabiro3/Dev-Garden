0ccbfc250124ffb2cd34d173c4e2c306235ab63c
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-18 16:15:08 +0000

    Finalized th platform, ignored the user onboarding dialog

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,36 +1,23 @@
-"use client"
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
 
-const Setup = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkProfileAndServer = async () => {
-      const profile = await initialProfile();
-      const server = await db.server.findFirst({
-        where: {
-          members: {
-            some: {
-              profileId: profile.id,
-            },
-          },
+const Setup = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
         },
-      });
+      },
+    },
+  });
 
-      const isWelcomeShown = localStorage.getItem("isWelcomeShown");
-      if (!isWelcomeShown) {
-        router.push("/welcome");
-      } else if (server) {
-        router.push(`/servers/${server.id}`);
-      }
-    };
-
-    checkProfileAndServer();
-  }, [router]);
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
 
   return (
     <div>
