fdb0eca69f51259071490bfc774889f05ea65dbf
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-19 11:48:19 +0000

    Video components

--- components/ui/video-demo.tsx
+++ components/ui/video-demo.tsx
@@ -25,12 +25,13 @@ const VideoOverlay = () => {
       {isOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
           <div className="relative w-full max-w-4xl mx-auto p-4">
-            <button
-              onClick={handleClose}
-              className="absolute left-0 bg-primary rounded-r-full transition-all w-[4px]"
-            >
-              Close
-            </button>
+          <button
+  onClick={handleClose}
+  className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 hover:bg-blue-700 text-white rounded-full p-2 transition-all shadow-lg"
+>
+  X
+</button>
+
             <video
               className="w-full h-auto max-h-[80vh] rounded-lg"
               controls
