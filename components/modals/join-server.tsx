5f208cf20783a342b4124d8e2e8a7ae31ab6a329
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-25 09:32:43 +0000

    Join server modal

---
+++ components/modals/join-server.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-model-store";
+
+export const InitialModal = () => {
+  const [isMounted, setMounted] = useState(false);
+  const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+  const isModalOpen = isOpen && type === "initial";
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const schema = z.object({
+    inviteLink: z.string().url({
+      message: "Please enter a valid URL",
+    }),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      inviteLink: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof schema>) => {
+    try {
+      // Here, we are assuming the server invite link will lead to a valid page
+      const response = await fetch(values.inviteLink);
+      if (!response.ok) {
+        throw new Error("Invalid Invitation Link");
+      }
+
+      // Navigate to the URL if valid
+      window.location.href = values.inviteLink;
+    } catch (error) {
+      alert("Invalid Invitation Link");
+    }
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">Join a Server</DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Enter an invite below to join an existing server
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 px-6">
+            <FormField
+              control={form.control}
+              name="inviteLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                    Invite Link *
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                      placeholder="https://discord.gg/example"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit" variant="primary" disabled={isLoading}>
+                Join Server
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InitialModal;
