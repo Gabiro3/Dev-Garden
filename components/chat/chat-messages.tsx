006c690ef57aac9ab85978e59b8e7ee77eb47b1d
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-27 05:02:21 +0000

    Added msg date separator above the messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -105,7 +105,7 @@ export const ChatMessages = ({
           )}
         </div>
       )}
-      <div className="flex flex-col-reverse mt-auto">
+      <div className="flex flex-col mt-auto">
       {data?.pages?.map((group, i) => (
   <Fragment key={i}>
     {group.items
